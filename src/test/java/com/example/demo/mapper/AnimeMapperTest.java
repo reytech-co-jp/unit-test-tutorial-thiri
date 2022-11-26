347393416655f3ae029767a956957f2e01e577e9
小山由人 <koyama@reytech.co.jp>
2022-11-26 15:00:22 +0900

    TestがFailするように修正

--- src/test/java/com/example/demo/mapper/AnimeMapperTest.java
+++ src/test/java/com/example/demo/mapper/AnimeMapperTest.java
@@ -4,16 +4,16 @@ import com.example.demo.entity.Anime;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
-import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @DBRider
 @MybatisTest
@@ -24,14 +24,14 @@ public class AnimeMapperTest {
 
     @Test
     @DataSet(value = "anime.yml")
-    void すべてのアニメが取得できること(){
+    void すべてのアニメが取得できること() {
         List<Anime> animeList = animeMapper.findAll();
         assertThat(animeList)
-                .hasSize(2)
-                .contains(
-                        new Anime(1, "Kill la Kill", "Action"),
-                        new Anime(2, "Fairy Tail", "Adventure")
-                );
+            .hasSize(3)
+            .contains(
+                new Anime(1, "Kill la Kill", "Action"),
+                new Anime(2, "Fairy Tail", "Adventure")
+            );
     }
 
     @Test
