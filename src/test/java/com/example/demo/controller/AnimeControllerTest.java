d8568d1a76727699e16233bed46217349bad5599
Thiri Yadanar <tryatanar@gmail.com>
2022-12-02 00:47:33 +0900

    create,update,deleteのテストケースを追加

--- src/test/java/com/example/demo/controller/AnimeControllerTest.java
+++ src/test/java/com/example/demo/controller/AnimeControllerTest.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.Anime;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.service.AnimeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,9 @@ public class AnimeControllerTest {
 
     @Autowired
     private JacksonTester<Anime> animeJacksonTester;
+
+    @Autowired
+    ObjectMapper mapper;
 
     @Test
     public void アニメが全件取得できること() throws Exception {
@@ -103,6 +107,94 @@ public class AnimeControllerTest {
 
         verify(animeService, times(1)).getAnime(1);
 
+    }
+
+    @Test
+    public void アニメの登録ができること() throws Exception {
+
+        Anime anime = new Anime("Your Name", "Romantic Fantasy");
+
+        String url = "/api/anime";
+
+        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(this.mapper.writeValueAsString(anime)))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse();
+
+        assertThat("anime successfully created").isEqualTo(response.getContentAsString());
+
+        verify(animeService, times(1)).registerAnime("Your Name", "Romantic Fantasy");
+    }
+
+    @Test
+    public void アニメの更新ができること() throws Exception {
+
+        Anime updatedAnime = new Anime(1,"Kill la Kill", "Action");
+
+        String url = "/api/anime/1";
+
+        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders.patch(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(this.mapper.writeValueAsString(updatedAnime)))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse();
+
+        assertThat("anime successfully updated").isEqualTo(response.getContentAsString());
+
+        verify(animeService, times(1)).updateAnime(1, "Kill la Kill", "Action");
+    }
+
+    @Test
+    public void アニメが更新できないときに例外をthrowすること() throws Exception {
+
+        doThrow(new ResourceNotFoundException("resource not found")).when(animeService).updateAnime(1, "Kill la Kill", "Action");
+
+        Anime anime = new Anime(1,"Kill la Kill", "Action");
+
+        String url = "/api/anime/1";
+
+        String error = mvc.perform(MockMvcRequestBuilders.patch(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(this.mapper.writeValueAsString(anime)))
+                .andExpect(status().isNotFound())
+                .andReturn().getResolvedException().getMessage();
+
+        assertThat(error).isEqualTo("resource not found");
+
+        verify(animeService, times(1)).updateAnime(1, "Kill la Kill", "Action");
+    }
+
+    @Test
+    public void アニメが削除できること() throws Exception {
+
+        String url = "/api/anime/1";
+
+        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders.delete(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse();
+
+        assertThat("anime successfully deleted").isEqualTo(response.getContentAsString());
+
+        verify(animeService, times(1)).deleteAnime(1);
+    }
+
+    @Test
+    public void アニメが削除できないときに例外をthrowすること() throws Exception {
+
+        doThrow(new ResourceNotFoundException("resource not found")).when(animeService).deleteAnime(1);
+
+        String url = "/api/anime/1";
+
+        String error = mvc.perform(MockMvcRequestBuilders.delete(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isNotFound())
+                .andReturn().getResolvedException().getMessage();
+
+        assertThat(error).isEqualTo("resource not found");
+
+        verify(animeService, times(1)).deleteAnime(1);
     }
 
 }
